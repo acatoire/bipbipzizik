78db7049f5ce0640e24ce8a5e2c376551e811b78
AxelC <axxcat@gmail.com>
2019-04-16 23:15:12 +0200

    Update sonosplay.sh
    Add the args read and curl command

--- sonosplay.sh
+++ sonosplay.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Get arguments
+addr=$1   # Mendatory - exemple : http://192.168.0.140:5005
+cmd=$2    # Mendatory - exemple : volume/50
+#where=$3 # Optional - exemple : salon TODO
+
+# Execut the sonos api request using curl
+curl -X GET $addr/$cmd todo add where as optional
+#curl -X GET $addr/$where/$cmd TODO add $where as optional
