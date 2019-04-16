2b7265c49c0bfaef2b355b74fb61f694df42e2d2
DESKTOP-DFRO5T8\Axel <axel.catoire@gmail.com>
2019-04-16 23:59:46 +0200

    Add room parameter option.

--- sonosplay.sh
+++ sonosplay.sh
@@ -3,8 +3,18 @@
 # Get arguments
 addr=$1   # Mendatory - exemple : http://192.168.0.140:5005
 cmd=$2    # Mendatory - exemple : volume/50
-#where=$3 # Optional - exemple : salon TODO
+where=$3  # Optional  - exemple : salon TODO
 
-# Execut the sonos api request using curl
-curl -X GET $addr/$cmd todo add where as optional
-#curl -X GET $addr/$where/$cmd TODO add $where as optional
+
+echo "Execute the command '$cmd' on the server at '$addr'"
+
+
+# Execute the sonos api request using curl
+if [ -z $where ]
+then
+    curl -X GET $addr/$cmd
+else
+    echo "only for the room $where"
+    curl -X GET $addr/$where/$cmd
+fi
+
