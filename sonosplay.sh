867b9ae13ea0bde37b9bab7bb39d4f0669cbf642
AxelC <axel.catoire@gmail.com>
2019-06-14 22:41:20 +0200

    Try to clear queu for playlist, not working

--- sonosplay.sh
+++ sonosplay.sh
@@ -12,9 +12,13 @@ echo "Execute the command '$cmd' on the server at '$addr'"
 # Execute the sonos api request using curl
 if [ -z $where ]
 then
+    # Clear the queu and load the new album/playlist
+    # Todo, check if needed for all (maybe not for tracks
+    # curl -X GET $addr/clearqueue
     curl -X GET $addr/$cmd
 else
     echo "only for the room $where"
+    # curl -X GET $addr/$where/clearqueue
     curl -X GET $addr/$where/$cmd
 fi
 
