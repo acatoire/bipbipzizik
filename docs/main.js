853039e924a606969df095ca454a6f912d65f065
AxelC <axel.catoire@gmail.com>
2020-02-07 22:46:57 +0100

    Read all cards elements

--- docs/main.js
+++ docs/main.js
@@ -8,12 +8,16 @@ var config = {
 firebase.initializeApp(config);
 var db = firebase.database();
 
-// CREATE REWIEW
+// CREATE CARD
 
 var cardForm = document.getElementById('cardForm');
-var name = document.getElementById('name');
-var name = document.getElementById('ids');
 var hiddenId   = document.getElementById('hiddenId');
+var name = document.getElementById('name');
+var ids = document.getElementById('ids');
+var action = document.getElementById('action');
+var data = document.getElementById('data');
+var mode = document.getElementById('mode');
+var comment = document.getElementById('comment');
 
 cardForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -24,15 +28,23 @@ cardForm.addEventListener('submit', (e) => {
 
   db.ref('cards_prod/' + id).set({
     name: name.value,
-    ids: ids.value
+    ids: ids.value,
+    action: action.value,
+    data: data.value,
+    mode: mode.value,
+    comment: comment.value
   });
 
+  hiddenId.value = '';
   name.value = '';
   ids.value  = '';
-  hiddenId.value = '';
+  action.value  = '';
+  data.value  = '';
+  mode.value  = '';
+  comment.value  = '';
 });
 
-// READ REVEIWS
+// READ CARDS
 
 var cards = document.getElementById('cards');
 var cardsRef = db.ref('/cards_prod');
@@ -45,36 +57,50 @@ cardsRef.on('child_added', (data) => {
 });
 
 cardsRef.on('child_changed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.innerHTML = cardTemplate(data.val());
+  var cardNode = document.getElementById(data.key);
+  cardNode.innerHTML = cardTemplate(data.val());
 });
 
 cardsRef.on('child_removed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.parentNode.removeChild(reviewNode);
+  var cardNode = document.getElementById(data.key);
+  cardNode.parentNode.removeChild(cardNode);
 });
 
 cards.addEventListener('click', (e) => {
-  var reviewNode = e.target.parentNode
+  var cardNode = e.target.parentNode
 
-  // UPDATE REVEIW
+  // UPDATE CARD
   if (e.target.classList.contains('edit')) {
-    name.value = reviewNode.querySelector('.name').innerText;
-    ids.value  = reviewNode.querySelector('.ids').innerText;
-    hiddenId.value = reviewNode.id;
+    name.value = cardNode.querySelector('.name').innerText;
+    ids.value  = cardNode.querySelector('.ids').innerText;
+    action.value = cardNode.querySelector('.action').innerText;
+    data.value  = cardNode.querySelector('.data').innerText;
+    mode.value = cardNode.querySelector('.mode').innerText;
+    comment.value  = cardNode.querySelector('.comment').innerText;
+    hiddenId.value = cardNode.id;
   }
 
-  // DELETE REVEIW
+  // DELETE CARD
   if (e.target.classList.contains('delete')) {
-    var id = reviewNode.id;
+    var id = cardNode.id;
     db.ref('cards_prod/' + id).remove();
   }
 });
 
-function cardTemplate({name, ids}) {
+function cardTemplate({name, ids, action, data, mode, comment}) {
   return `
-    <div class='fullName'>${name}</div>
-    <div class='message'>${ids}</div>
+    <div class='nameLabel'>Name:</div>
+    <div class='name'>${name}</div>
+    <div class='idsLabel'>Ids:</div>
+    <div class='ids'>${ids}</div>
+    <div class='actionLabel'>Action:</div>
+    <div class='action'>${action}</div>
+    <div class='dataLabel'>Data:</div>
+    <div class='data'>${data}</div>
+    <div class='modeLabel'>Mode:</div>
+    <div class='mode'>${mode}</div>
+    <div class='commentLabel'>Comment:</div>
+    <div class='comment'>${comment}</div>
     <button class='delete'>Delete</button>
     <button class='edit'>Edit</button>
   `
