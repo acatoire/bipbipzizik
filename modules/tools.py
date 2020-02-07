f4c1cc3cfe0135d80b915ac9bbe13cc5f17047ac
AxelC <axel.catoire@gmail.com>
2020-02-07 22:46:15 +0100

    Some pylint

--- modules/tools.py
+++ modules/tools.py
@@ -1,22 +1,32 @@
+"""
+Module containing useful tools
+"""
 
+# pylint: disable=import-outside-toplevel
 
 def print_my_path():
+    """
+    For debug purpose, print path
+    :return: None
+    """
     from os import path, getcwd
     print('DEBUG: cwd:     {}'.format(getcwd()))
     print('DEBUG: __file__:{}'.format(__file__))
     print('DEBUG: abspath: {}'.format(path.abspath(__file__)))
 
 
-def get_serial():
-    # Extract serial from cpuinfo file
+def get_serial() -> str:
+    """
+    Extract serial from raspberry pi cpuinfo file
+    :return: serial as string
+    """
     serial = "0000000000000000"
     try:
-        f = open('/proc/cpuinfo', 'r')
-        for line in f:
-            if line[0:6] == 'Serial':
-                serial = line[10:26]
-        f.close()
-    except:
-        serial = "ERROR000000000"
+        with open('/proc/cpuinfo', 'r') as file_handler:
+            for line in file_handler:
+                if line[0:6] == 'Serial':
+                    serial = line[10:26]
+    except FileNotFoundError:
+        serial = "ERROR00000000000"
 
     return serial
